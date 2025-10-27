a8069a8169bd672735b70f6d2bf2864a923827cd
CellarKid <S.Schleidt@s3t.de>
2025-10-27 12:21:13 +0100

    test: remove unnecessary files for testing bitmap_tracer

--- sketchgetdp/bitmap_tracer/tests/shared/doubles/mocks/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Exposes mock implementations for SVG generation and bitmap tracing testing.
-
-These mocks facilitate isolated unit tests by providing controlled, predictable responses.
-"""
-from .svg_mocks import *
-from .tracing_mocks import *
